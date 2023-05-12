568e3389ac0277e4fbe34519ecd6c73239f06bed
FBH514 <103460138+FBH514@users.noreply.github.com>
2023-05-12 14:24:05 -0400

    Touchup

--- install.sh
+++ install.sh
@@ -180,7 +180,7 @@ function create_client {
 
   export default function App(): JSX.Element {
     return (
-        <div className={"App"}>
+        <div className={'App'}>
             FBH
         </div>
     );
