2ba422ddb99dce4ee2adece4af0a762d1f61cbff
Francois Boulay-Handfield <103460138+fbhdev@users.noreply.github.com>
2023-10-02 08:20:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -201,7 +201,42 @@ function create_client {
   }
   " >>useKeyboard.ts
   cd ../utils || exit
-  touch Icons.tsx Keyboard.ts Requests.ts Animate.ts Cache.ts
+  touch Icons.tsx ProjectRoutes.tsx Keyboard.ts Requests.ts Animate.ts Cache.ts
+  echo "
+  export class Cache {
+
+  private static readonly loot = {};
+
+  static get keys() {
+    return this.loot;
+  }
+
+  static getItem(key: string): any {
+    if (!key) {
+      return null;
+    }
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  }
+
+  static setItem(key: string, value: unknown): void {
+    if (!key && !value) {
+      return;
+    }
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static removeItem(key: string): void {
+    if (!key) {
+      return;
+    }
+    localStorage.removeItem(key)
+  }
+
+  static clearCache(): void {
+    localStorage.clear();
+  }
+}" >> Cache.ts
   echo "
   const enum ENDPOINTS {}
   const enum QUERY_KEYS {}
