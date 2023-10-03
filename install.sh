104b4ef179082fc214f34adc6c5fa96622975823
FBH514 <103460138+FBH514@users.noreply.github.com>
2023-10-02 21:18:11 -0400

    Clean up

--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
 #!/bin/zsh
 
-# create server directory with necessary files
-function create_server {
-  mkdir server || exit
-  cd server || exit
-  touch resource.py process.py project.py utils.py database.py .env
-  python3.9 -m venv venv || exit
-  source venv/bin/activate
-  pip3 install fastapi
-  pip3 install uvicorn
-  pip3 install python-dotenv
-  pip3 install icecream
-  pip3 install pymongo
+# Server functions
+function create_resource {
   echo "
 import functools
 import os
@@ -21,6 +11,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import Response
 from starlette.requests import Request
 from dotenv import load_dotenv
+from process import Process
+from project import Project
 
 load_dotenv()
 app = FastAPI()
@@ -46,74 +38,189 @@ def cache(seconds: int) -> callable:
 
 
 # GET http://localhost:8000/
-@cache(60**2*24*7)
+@cache(Project.CACHE_TIME)
 @app.get('/')
 async def root():
     return {'message': 'FBH'}
-" >>resource.py
+  " >>resource.py
+}
 
-echo "
-import sqlite3
-from typing import Optional, List
+function create_process {
+  echo "
+import os
+from dotenv import load_dotenv
+from database import Database
+from utils import Status
+from template import Template
 
+class Process:
+
+  load_dotenv()
+  _MONGO_URI: str = os.getenv('MONGO_UR')
+  _DB_NAME: str = os.getenv('DB_NAME')
+  _INSTANCE: 'Process' = None
+  _db = Database(_MONGO_URI, _DB_NAME)
+
+  def __new__(cls, *args, **kwargs) -> 'Process':
+      if not cls._INSTANCE:
+          cls._instance = super(Process, cls).__new__(cls, *args, **kwargs)
+      return cls._INSTANCE
+  ">>process.py
+}
+
+function create_utils {
+  echo "
+class Status:
+
+  OK = 200
+  CREATED = 201
+  ACCEPTED = 202
+  NO_CONTENT = 204
+
+  BAD_REQUEST = 400
+  UNAUTHORIZED = 401
+  FORBIDDEN = 403
+  NOT_FOUND = 404
+
+  INTERNAL_SERVER_ERROR = 500
+  ">>utils.py
+}
+
+function create_response_template {
+  echo "
+class Template:
+
+    """"""
+    @staticmethod
+    def generate(status_code: int, results: list = None, message: str = None) -> dict:
+        """"""
+        return {
+            'status_code': status_code,
+            'results': results,
+            'message': message
+        }
+  ">>template.py
+}
+
+function create_database {
+  echo "
+import pymongo
+from pymongo.results import UpdateResult, DeleteResult, InsertOneResult
+from bson import ObjectId
 
 class Database:
 
-  def __init__(self, db_name: str) -> None:
-      if not db_name.endswith('.db'):
-          raise ValueError('{} must end with .db'.format(db_name))
-      self.name = db_name
-      self.conn = self.connect()
-      self.cur = self.cursor()
+    def __init__(self, url: str, db_name: str):
+        self.client = pymongo.MongoClient(url)
+        self.database = self.client[db_name]
+        self._collection = None
 
-  def __enter__(self) -> 'Database':
-      return self
+    def __enter__(self) -> 'Database':
+        return self
 
-  def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-      self.conn.close()
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.client.close()
 
-  def connect(self) -> sqlite3.Connection:
-      if not self.name:
-          raise ValueError('Database name not found')
-      return sqlite3.connect(self.name)
+    @property
+    def collection(self) -> pymongo.collection.Collection:
+        return self._collection
 
-  def cursor(self) -> sqlite3.Cursor:
-      if not self.conn:
-          raise ValueError('Connection not found')
-      return self.conn.cursor()
+    @collection.setter
+    def collection(self, value: str) -> None:
+        self._collection = self.database[value]
 
-  def commit(self) -> None:
-      if not self.conn:
-          raise ValueError('Connection not found')
-      self.conn.commit()
+    async def insert_document(self, data: dict) -> InsertOneResult:
+        return self.collection.insert_one(data)
 
-  def execute(self, sql: str, data: dict = None) -> Optional[List[tuple]]:
-      if not self.cur:
-          raise ValueError('Cursor not found')
-      if data:
-          self.cur.execute(sql, data)
-      else:
-          self.cur.execute(sql)
-      self.commit()
-      return self.cur.fetchall()
-  " >> database.py
+    async def find_documents(self, query=None) -> list:
+        if query is None:
+            query = {}
+        results = list(self.collection.find(query))
+        return await Database.convert_ids(results)
+
+    @staticmethod
+    async def convert_ids(results: list) -> list:
+        for item in results:
+            if not isinstance(item['_id'], ObjectId):
+                continue
+            item['_id'] = str(item['_id'])
+        return results
+
+    async def update_document(self, query: dict, data: dict, delete: bool = False) -> UpdateResult:
+        return self.collection.update_one(
+            query, {'$unset': data} if delete else {'$set': data}
+        )
+
+    async def delete_document(self, query: dict) -> DeleteResult:
+        return self.collection.delete_one(query)
+
+    async def delete_many(self) -> DeleteResult:
+        return self.collection.delete_many({})
+
+    async def count_documents(self, query=None) -> int:
+        if query is None:
+            query = {}
+        return self.collection.count_documents(query)
+  " >>database.py
+}
+
+function env_variables {
+  echo "
+MONGO_URI=''
+DB_NAME=''
+  " >>.env
+}
+
+function create_project {
+  echo "
+class Project:
+  NAME: str = ''
+  VERSION: str = ''
+  CACHE_TIME: int = (60**2*7*24)
+  ">>project.py
+}
+
+function create_server {
+  mkdir server || exit
+  cd server || exit
+  touch resource.py process.py project.py utils.py database.py .env
+  python3 -m venv venv || exit
+  source venv/bin/activate
+  pip3 install fastapi
+  pip3 install uvicorn
+  pip3 install python-dotenv
+  pip3 install icecream
+
+  env_variables
+  create_resource
+  create_process
+  create_utils
+  create_response_template
+  create_database
+  create_project
   cd ../ || exit
 }
 
-# create client directory with necessary files
-function create_client {
+# Client functions
+function setup_vite {
   yarn create vite client --template react-ts
   cd client || exit
   touch yarn.lock
   yarn
+}
+
+function install_client_dependencies {
   yarn add react-query
   yarn add react-router-dom
   yarn add framer-motion
   yarn add @fortawesome/fontawesome-svg-core \
-  @fortawesome/free-solid-svg-icons \
-  @fortawesome/free-brands-svg-icons \
-  @fortawesome/free-regular-svg-icons \
-  @fortawesome/react-fontawesome
+    @fortawesome/free-solid-svg-icons \
+    @fortawesome/free-brands-svg-icons \
+    @fortawesome/free-regular-svg-icons \
+    @fortawesome/react-fontawesome
+}
+
+function setup_tailwindcss {
   yarn add -D tailwindcss postcss autoprefixer
   touch tailwind.config.cjs
   touch postcss.config.cjs
@@ -148,14 +255,16 @@ function create_client {
       },
       plugins: [],
   }" >>tailwind.config.cjs
+}
+
+function structure_client_project {
   rm -rf src || exit
   mkdir src || exit
   cd src || exit
   mkdir types utils hooks constants components styles assets
-  touch index.css main.tsx App.tsx
-  cd components || exit
-  mkdir nav ui c1 c2 c3
-  cd ../ || exit
+}
+
+function create_base_types {
   cd types || exit
   touch Base.ts
   echo "
@@ -163,9 +272,12 @@ function create_client {
     mobile?: boolean;
     className?: string;
     isModal?: boolean;
-    setModal?: (value: boolean) => void;
-  }" >> Base.ts
+    setIsModal?: (value: boolean) => void;
+  }" >>Base.ts
   cd ../ || exit
+}
+
+function create_base_hooks {
   cd hooks || exit
   touch useKeyboard.ts useMobile.ts
   echo "
@@ -200,43 +312,12 @@ function create_client {
       })
   }
   " >>useKeyboard.ts
-  cd ../utils || exit
-  touch Icons.tsx ProjectRoutes.tsx Keyboard.ts Requests.ts Animate.ts Cache.ts
-  echo "
-  export class Cache {
+  cd ../ || exit
+}
 
-  private static readonly loot = {};
-
-  static get keys() {
-    return this.loot;
-  }
-
-  static getItem(key: string): any {
-    if (!key) {
-      return null;
-    }
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
-  }
-
-  static setItem(key: string, value: unknown): void {
-    if (!key && !value) {
-      return;
-    }
-    localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  static removeItem(key: string): void {
-    if (!key) {
-      return;
-    }
-    localStorage.removeItem(key)
-  }
-
-  static clearCache(): void {
-    localStorage.clear();
-  }
-}" >> Cache.ts
+function create_client_utils {
+  cd utils || exit
+  touch Icons.tsx Keyboard.ts Requests.ts Animate.ts
   echo "
   const enum ENDPOINTS {}
   const enum QUERY_KEYS {}
@@ -278,6 +359,9 @@ function create_client {
   }
   " >>requests.ts
   cd ../ || exit
+}
+
+function create_client_styles {
   echo "
   /* https://tailwindcss.com/docs/customizing-colors */
   @import url('https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap');
@@ -291,6 +375,10 @@ function create_client {
   }
 
   " >>index.css
+}
+
+function create_client_entry {
+  touch index.css main.tsx App.tsx
   echo "
   import React from 'react';
   import ReactDOM from 'react-dom/client';
@@ -308,20 +396,26 @@ function create_client {
     </React.StrictMode>
   );" >>main.tsx
   echo "
-  import React from "react";
-  import {BaseComponent} from "./types/Base.ts";
-  
-  
-  const App: React.FC<BaseComponent> = () => {
+  export default function App(): JSX.Element {
     return (
         <div className={'App'}>
             {'FBH'}
         </div>
     );
-  }
-  App.displayName = "App";
-  export default React.memo(App);
-  " >>App.tsx
+  }" >>App.tsx
+}
+
+# Execution of client creation functions
+function create_client {
+  setup_vite
+  install_client_dependencies
+  setup_tailwindcss
+  structure_client_project
+  create_base_types
+  create_base_hooks
+  create_client_utils
+  create_client_styles
+  create_client_entry
 }
 
 create_server &
